cdd945e93842683972d9b8491e08014d3291ebc2
skreebydba <skreebydba@users.noreply.github.com>
2016-07-11 15:03:29 -0500

    Update Test-DatabaseRestore.ps1
    Added logic to check if a single backup file is returned.

--- Test-DatabaseRestore.ps1
+++ Test-DatabaseRestore.ps1
@@ -8,66 +8,42 @@ function Test-DatabaseRestore {
 .SYNOPSIS
 For the database input, restores the most recent FULL backup, all DIFF backup since the most recent FULL, and all LOG backups
 since the most recent DIFF 
-
 .DESCRIPTION
 This function identifies the most recent FULL backup file for a database, any DIFF backup files that have been generated since the 
 last FULL backup for that database, and any LOG backup files since the most recent DIFF
 It then runs the Restore-SQLDatabase cmdlet for each file, setting the -norecovery flag for each file except the last one which runs
 WITH RECOVERY
-
 .EXAMPLE
-
 Test-DatabaseRestore -databasename YourDatabase -backupinstance YourBackupInstance -restoreinstance YourRestoreInstance -noexec 0
-
 Description
 -----------
-
 This example will identify the valid backup files for YourDatabase on YourBackupInstance and restore those files to YourRestoreInstance
-
 .EXAMPLE
-
 Test-DatabaseRestore -databasename YourDatabase -backupinstance YourBackupInstance -restoreinstance YourRestoreInstance -restorepath C:\YourRestorePath -noexec 0
-
 Description
 -----------
-
 This example will identify the valid backup files for YourDatabase on YourBackupInstance and restore those files to YourRestoreInstance
 If -backupinstance and -restoreinstance are not equal, all backup files will be copied to C:\YourRestorePath on YourRestoreInstance
-
 .EXAMPLE
-
 Test-DatabaseRestore -databasename YourDatabase -backupinstance YourBackupInstance -restoreinstance YourRestoreInstance -restorepath C:\YourRestorePath -stopatdate "2016-06-05 12:59:31" -noexec 0
-
 Description
 -----------
-
 This example will identify the valid backup files for YourDatabase on YourBackupInstance before "2016-06-05 12:59:31" and copy those files to C:\YourRestorePath on YourRestoreInstance
 The -stopatdate value will be used in the last log restore
-
 .EXAMPLE
-
 Test-DatabaseRestore -databasename YourDatabase -backupinstance YourBackupInstance -restoreinstance YourRestoreInstance -restorepath C:\YourRestorePath -stopatdate "2016-06-05 12:59:31" -noexec 0
-
 Description
 -----------
-
 This example will identify the valid backup files for YourDatabase on YourBackupInstance before "2016-06-05 12:59:31" and copy those files to C:\YourRestorePath on YourRestoreInstance
 The -stopatdate value will be used in the last log restore
-
 .EXAMPLE
-
 Test-DatabaseRestore -databasename YourDatabase -backupinstance YourBackupInstance -restoreinstance YourRestoreInstance -restorepath C:\YourRestorePath -noexec 1
-
 Description
 -----------
-
 This example will identify the valid backup files for YourDatabase on YourBackupInstance and copy those files to C:\YourRestorePath on YourRestoreInstance
 The Restore-SqlDatabase commands will be generated and written to file C:\YourRestorePath\restore.sql
-
-
 .PARAMETER databasename
 The database to test restores for
-
 #>
 [CmdletBinding()]
 param
@@ -141,7 +117,6 @@ process {
     }
     <#
     [string]$setsingle = "ALTER DATABASE $databasename SET SINGLE_USER WITH ROLLBACK IMMEDIATE";
-
     if($noexec -eq 1)
     {
         Write-Output "Invoke-Sqlcmd -Query '$setsingle' -Database 'master' -ServerInstance $restoreinstance;"
@@ -161,10 +136,10 @@ process {
         [string]$querystring = "EXEC master.dbo.GetRestoreFiles @db_name = '$databasename', @stopat = '$stopatdate'";
     }
 
-    $querystring;
+    #$querystring;
 
     <# Execute the stored procedure and pass the result set to variable `$filelist #>
-    $filelist = Invoke-Sqlcmd -Query $querystring -ServerInstance $backupinstance -database "master" 
+    $filelist = @(Invoke-Sqlcmd -Query $querystring -ServerInstance $backupinstance -database "master")
 
     $filelist;
 
@@ -175,9 +150,18 @@ process {
     }
         
     <# Initialize the loop counter and loop limit #>
-    $filecount = 0;
-    $filelimit = ($filelist.Length - 1);
-
+    if($filelist.Length)
+    {
+        $filecount = 0;
+        $filelimit = ($filelist.Length - 1)
+        $array = 1;
+    }
+    else
+    {
+        $filecount = 0;
+        $filelimit = 1;
+        $array = 0;
+    }
     if ($restoreinstance.Contains("\"))
     {
         $restoreserver = $restoreinstance.Substring(0,($restoreinstance.IndexOf("\")));
@@ -228,10 +212,18 @@ process {
     <# Loop through each backup file and build Restore-SqlDatabase commands #>
     while($filecount -le $filelimit)
     {
-
-        $file = $filelist[$filecount].physical_device_name;
-        $type = $filelist[$filecount].type;
-
+        $filelimit;
+        if($array -eq 0)
+        {
+            $file = $filelist.physical_device_name;
+            $type = $filelist[$filecount].type;
+            $filecount = 2;
+        }
+        else
+        {
+            $file = $filelist[$filecount].physical_device_name;
+            $type = $filelist[$filecount].type;
+        }
         $backupunc = "\\" + $backupserver + "\" + $file.Replace(":","$")
 
         <# If the backup instance is not the same as the restore instance, get the RelocateFile for the restore instance #>
@@ -481,4 +473,154 @@ process {
     $finaltotaldur | Out-File $durationfile -Append;
 
 }
+}
+function Get-RelocateFile {
+<#
+.SYNOPSIS
+Builds the RelocateFile parm for Restore-SQLDatabase
+
+.DESCRIPTION
+This cmdlet identifies the default data and log file locations for a SQL instance and uses that information
+to build the RelocateFile parameter for the REstore-SQLDatabase cmdlet
+
+.EXAMPLE
+Get-RelocateFile -databasename YourDatabaseName -restoreinstance TheInstanceYouAreRestoringTo -backupfile YourBackupFile
+
+.PARAMETER databasename
+The database to test restores for
+
+.PARAMETER restoreinstance
+The instance you will be restoring to 
+
+.PARAMETER backup file
+The backup file you are restoring
+
+#>
+[CmdletBinding()]
+param
+(
+[Parameter(Mandatory=$True,
+Position = 1,
+ValueFromPipeline=$True,
+ValueFromPipelineByPropertyName=$True,
+    HelpMessage='What database do you want to test restores for?')]
+[Alias('database')]
+[string]$databasename,
+
+[Parameter(Mandatory=$True,
+Position = 1,
+ValueFromPipeline=$True,
+ValueFromPipelineByPropertyName=$True,
+    HelpMessage='What instance do you want to restore the backups to?')]
+[Alias('restoreinst')]
+[string]$restoreinstance,
+
+    [Parameter(Mandatory=$False,
+ValueFromPipeline=$True,
+ValueFromPipelineByPropertyName=$True,
+    HelpMessage='Set to 1 if you want to execute the database restore.  Otherwise ')]
+[Alias('bfile')]
+[string]$backupfile 
+)
+
+process {
+        
+    <#
+    The code in this function has been modified from the code at 
+    https://www.simple-talk.com/sql/backup-and-recovery/backup-and-restore-sql-server-with-the-sql-server-2012-powershell-cmdlets/
+    Many thanks to Allen White for making it generally available
+    #>
+
+    cls;
+
+    # Connect to the specified instance
+    $srv = new-object ('Microsoft.SqlServer.Management.Smo.Server') $restoreinstance;
+
+ 
+
+    # Get the default file and log locations
+
+    # (If DefaultFile and DefaultLog are empty, use the MasterDBPath and MasterDBLogPath values)
+
+    $fileloc = $srv.Settings.DefaultFile
+
+    $logloc = $srv.Settings.DefaultLog
+
+    if ($fileloc.Length -eq 0) {
+
+        $fileloc = $srv.Information.MasterDBPath
+
+        }
+
+    if ($logloc.Length -eq 0) {
+
+        $logloc = $srv.Information.MasterDBLogPath
+
+        }
+
+    #$fileloc;
+    #$logloc;
+ 
+
+    # $backupfile and $databasename are passed in as mandatory parameters
+
+    # Build the physical file names for the database copy
+
+    $dbfile = $fileloc + $databasename + '_Data.mdf'
+
+    $logfile = $logloc + $databasename + '_Log.ldf'
+
+ 
+
+    # Use the backup file name to create the backup device
+
+    $bdi = new-object ('Microsoft.SqlServer.Management.Smo.BackupDeviceItem') ($backupfile, 'File')
+
+ 
+
+    # Create the new restore object, set the database name and add the backup device
+
+    $rs = new-object('Microsoft.SqlServer.Management.Smo.Restore')
+
+    $rs.Database = $databasename
+
+    $rs.Devices.Add($bdi)
+
+    # Get the file list info from the backup file
+
+    $fl = $rs.ReadFileList($srv)
+
+    $rfl = @()
+
+    foreach ($fil in $fl) {
+
+        $rsfile = new-object('Microsoft.SqlServer.Management.Smo.RelocateFile')
+
+        $rsfile.LogicalFileName = $fil.LogicalName
+
+        if ($fil.Type -eq 'D') {
+
+            $rsfile.PhysicalFileName = $dbfile
+
+            }
+
+        else {
+
+            $rsfile.PhysicalFileName = $logfile
+
+            }
+
+        $rfl += $rsfile
+
+        }
+        
+    return $rfl;
+        
+
+ 
+    #Restore the database
+
+    #Restore-SqlDatabase -ServerInstance ".\instance2" -Database $databasename -BackupFile $bckfile -RelocateFile $rfl -ReplaceDatabase;
+
+    }  
 }
